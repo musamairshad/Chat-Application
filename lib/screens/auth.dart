f0fb891a797ee86371cef6f8cfd6316b762b8e70
Muhammad Usama <imusamairshad@gmail.com>
2023-08-16 23:43:34 +0500

    auth.dart is modified.

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:chat_app/widgets/user_image_picker.dart';
 
 final _firebase = FirebaseAuth.instance;
 
@@ -83,6 +84,7 @@ class _AuthScreenState extends State<AuthScreen> {
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
+                          if (!_isLogin) const UserImagePicker(),
                           TextFormField(
                             decoration: const InputDecoration(
                               label: Text('Email Address'),
