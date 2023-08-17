3e7126535a685db86da410907a2bfa26e50333ca
Muhammad Usama <imusamairshad@gmail.com>
2023-08-17 23:42:13 +0500

    user_image_picker.dart is modified.

--- lib/widgets/user_image_picker.dart
+++ lib/widgets/user_image_picker.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
-  const UserImagePicker({super.key});
+  final void Function(File pickedImage) onPickImage;
+  const UserImagePicker({super.key, required this.onPickImage});
 
   @override
   State<UserImagePicker> createState() {
@@ -28,6 +29,8 @@ class _UserImagePickerState extends State<UserImagePicker> {
     setState(() {
       _pickedImageFile = File(pickedImage.path);
     });
+
+    widget.onPickImage(_pickedImageFile!);
   }
 
   @override
